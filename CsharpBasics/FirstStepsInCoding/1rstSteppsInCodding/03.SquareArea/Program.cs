9fbc41c8d4985f893fb4bafcdf48f19480f02608
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-01-29 23:02:38 +0200

    Add from 0-6 problems

---
+++ CsharpBasics/FirstStepsInCoding/1rstSteppsInCodding/03.SquareArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            var side = int.Parse(Console.ReadLine());
+            var area = side * side;
+            Console.Write("");
+            Console.WriteLine(area);
+        }
+    }
+}
