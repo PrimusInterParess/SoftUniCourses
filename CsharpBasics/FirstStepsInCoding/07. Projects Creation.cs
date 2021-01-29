4094e59ada51199f56ba3780b8da95605c2cdc9c
Dani Borisov <77731733+DAniBorisovJB@users.noreply.github.com>
2021-01-29 18:12:38 +0200

    Add files via upload

---
+++ CsharpBasics/FirstStepsInCoding/07. Projects Creation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int projectNumber = int.Parse(Console.ReadLine());
+            int project = 3 * projectNumber;
+            Console.WriteLine($"The architect {architectName} will need {project} hours to complete {projectNumber} project/s.");
+        }
+    }
+}
