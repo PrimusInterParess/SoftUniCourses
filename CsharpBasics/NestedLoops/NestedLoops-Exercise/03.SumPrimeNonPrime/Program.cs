b31f8e11e9bb5b9d62cd3dc1507fb79f9c2d0295
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-01-30 15:53:50 +0200

    Add solutions from Nested Loops Exercise

---
+++ CsharpBasics/NestedLoops/NestedLoops-Exercise/03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _03.SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeNumbersSum = 0;
+            int nonPrimeNumbers = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    break;
+                }
+
+                int number = int.Parse(input);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    bool isPrime = true;
+                    int numberSqrt = (int)Math.Sqrt(number);
+
+                    for (int i = 2; i <= numberSqrt; i++)
+                    {
+                        if (number % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+
+                    }
+                    if (isPrime)
+                    {
+                        primeNumbersSum += number;
+                    }
+                    else
+                    {
+                        nonPrimeNumbers += number;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeNumbersSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeNumbers}");
+        }
+    }
+}
---
+++ CsharpBasics/NestedLoops/NestedLoops-Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalGrades = 0;
+            int presentationCounter = 0;
+            int numberOfGrades = int.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                string presentationName = Console.ReadLine();
+
+                if (presentationName == "Finish")
+                {
+                    break;
+                }
+
+                double gradeSum = 0;
+
+                for (int i = 0; i < numberOfGrades; i++)
+                {
+                    double grades = double.Parse(Console.ReadLine());
+                    presentationCounter++;
+                    gradeSum += grades;
+
+                }
+                totalGrades += gradeSum;
+
+                double average = gradeSum / numberOfGrades;
+                Console.WriteLine($"{presentationName} - {average:F2}.");
+            }
+
+            double final = totalGrades / presentationCounter;
+
+            Console.WriteLine($"Student's final assessment is {final:F2}.");
+        }
+    }
+}
