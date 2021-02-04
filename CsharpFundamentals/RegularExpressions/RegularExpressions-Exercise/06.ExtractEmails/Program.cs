deef6eb3670405e94bf3b72d1bf679801f65daa3
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-04 17:37:06 +0200

    Add solution

---
+++ CsharpFundamentals/RegularExpressions/RegularExpressions-Exercise/06.ExtractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s)(?<user>[A-Za-z0-9]+[.-]*\w)*@(?<host>[a-z]+?([.-][a-z]*)*(\.[a-z]{2,3}))";
+
+            string input = Console.ReadLine();
+
+            MatchCollection emailCollection = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(Environment.NewLine,emailCollection));
+        }
+    }
+}
