b46309eb29508edad618c047e0c44d9723379500
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-14 16:13:34 +0200

    add projects 7,8

---
+++ CsharpAdvanced/StacksAndQueues/StacksAndQueues-Exercise/07.TruckTour/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPetrolStation = int.Parse(Console.ReadLine());
+
+            Queue<string> petrolStationStop = new Queue<string>();
+
+            for (int i = 0; i < numberOfPetrolStation; i++)
+            {
+                string stationsData = Console.ReadLine();
+
+                petrolStationStop.Enqueue(stationsData + $" {i}");
+
+            }
+
+            int totalPetrol = 0;
+
+
+            for (int i = 0; i < numberOfPetrolStation; i++)
+            {
+                string info = petrolStationStop.Dequeue();
+
+                int[] currentInfo = info.Split().Select(int.Parse).ToArray();
+
+                int petrol = currentInfo[0];
+                int distance = currentInfo[1];
+
+                totalPetrol += petrol;
+
+                if (totalPetrol >= distance)
+                {
+                    totalPetrol -= distance;
+                }
+                else
+                {
+                    totalPetrol = 0;
+                    i = -1;
+                }
+
+                petrolStationStop.Enqueue(info);
+            }
+
+            string[] indexStrings = petrolStationStop.Dequeue().Split().ToArray();
+
+            Console.WriteLine(indexStrings[2]);
+
+        }
+
+
+    }
+}
---
+++ CsharpAdvanced/StacksAndQueues/StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Dictionary<char, char> pairsParentheses = new Dictionary<char, char>()
+            {
+                { '(',')'},{'{','}'},{'[',']'}
+            };
+
+            if (expression.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Stack<char> parenthesesChars = new Stack<char>();
+
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    parenthesesChars.Push(ch);
+
+                }
+                else if (parenthesesChars.Count == 0)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                else
+                {
+                    char lastBracket = parenthesesChars.Pop();
+
+                    char expected = pairsParentheses[lastBracket];
+
+                    if (ch != expected)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(parenthesesChars.Count == 0 ? "YES" : "NO");
+        }
+    }
+}
