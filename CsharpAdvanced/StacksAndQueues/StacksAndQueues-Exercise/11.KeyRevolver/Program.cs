2a445b6b37c20fb4b12ee1cf5cca8061075ecedb
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-17 15:58:11 +0200

    On to the next One

---
+++ CsharpAdvanced/StacksAndQueues/StacksAndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            // starts to shoot the locks front-to - back, while going through the bullets back-to - front.
+            //  If the bullet has a smaller or equal size to the current lock, print “Bang!”, then remove the lock.
+            // If not, print "Ping!",
+            // leaving the lock intact.The bullet is removed in both cases.
+            //    If Sam runs out of bullets in his barrel, print "Reloading!" on the console,
+            // then continue shooting.If there aren’t any bullets left, don’t print it.
+            //    The program ends when Sam either runs out of bullets, or the safe runs out of locks.
+
+
+            int bulletPrice = int.Parse(Console.ReadLine());
+
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+
+            int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int reword = int.Parse(Console.ReadLine());
+
+            Stack<int> bulletStack = new Stack<int>(bullets);
+
+            Queue<int> locksQueue = new Queue<int>(locks);
+
+            bool finishedBullets = false;
+
+            int barelLimit = gunBarrelSize;
+
+            int usedBullets = 0;
+
+            while (locksQueue.Count != 0)
+            {
+                if (bulletStack.Count == 0)
+                {
+                    break;
+                }
+
+                int currentBullet = bulletStack.Pop();
+
+                usedBullets++;
+                barelLimit--;
+
+
+
+                if (locksQueue.Peek() >= currentBullet)
+                {
+                    Console.WriteLine("Bang!");
+                    locksQueue.Dequeue();
+
+                    if (barelLimit == 0)
+                    {
+                        if (bulletStack.Count == 0)
+                        {
+                            finishedBullets = true;
+
+                            break;
+                        }
+                        else
+                        {
+                            barelLimit = gunBarrelSize;
+
+                            Console.WriteLine("Reloading!");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+
+
+                    if (barelLimit == 0)
+                    {
+                        if (bulletStack.Count == 0)
+                        {
+                            finishedBullets = true;
+
+                            break;
+                        }
+                        else
+                        {
+                            barelLimit = gunBarrelSize;
+
+                            Console.WriteLine("Reloading!");
+                        }
+                    }
+                }
+            }
+
+            int earnings = reword - (bulletPrice * usedBullets);
+
+            if (locksQueue.Count == 0)
+            {
+                Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earnings}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+            }
+
+        }
+    }
+}
---
+++ CsharpAdvanced/StacksAndQueues/StacksAndQueues-Exercise/12.CupsBottles/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.CupsBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] cupsCapacity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] bottlesVolume = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> bottleStack = new Stack<int>(bottlesVolume);
+
+            Queue<int> cupQueue = new Queue<int>(cupsCapacity);
+
+            int wastedWater = 0;
+
+            int bottleCounter = 0;
+
+            int cupCounter = 0;
+
+            while (cupQueue.Count != 0)
+            {
+
+                if (bottleStack.Count == 0)
+                {
+                    break;
+                }
+                int currentBottle = bottleStack.Pop();
+
+                bottleCounter++;
+
+                int currentCup = cupQueue.Dequeue();
+
+                cupCounter++;
+
+                if (currentCup > currentBottle)
+                {
+                    while (currentCup > 0)
+                    {
+                        currentCup -= currentBottle;
+
+                        if (currentCup > 0)
+                        {
+                            currentBottle = bottleStack.Pop();
+
+                            bottleCounter++;
+                        }
+
+                        if (currentCup < 0)
+                        {
+                            wastedWater += Math.Abs(currentCup);
+
+                        }
+                    }
+                }
+                else
+                {
+                    wastedWater += currentBottle -= currentCup;
+                }
+
+            }
+
+            //  If you have managed to fill up all of the cups, print the remaining water bottles,
+            // from the last entered – to the first,
+            // otherwise you must print the remaining cups, by order of entrance – from the first entered – to the last.
+
+            if (cupQueue.Count == 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottleStack)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupQueue)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+
+            }
+
+        }
+    }
+}
