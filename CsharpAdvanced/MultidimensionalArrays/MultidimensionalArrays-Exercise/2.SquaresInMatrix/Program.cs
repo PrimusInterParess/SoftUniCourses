540425f33182291bd12325a096d6ed250b686b5d
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-19 21:36:34 +0200

    add

---
+++ CsharpAdvanced/MultidimensionalArrays/MultidimensionalArrays-Exercise/2.SquaresInMatrix/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 2;
+
+            int[] dimentions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            char[,] charMatrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] charArray = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    charMatrix[row, col] = charArray[col];
+                }
+            }
+
+            int squareCount = 0;
+
+            for (int row = 0; row < rows - size + 1; row++)
+            {
+                for (int col = 0; col < cols - size + 1; col++)
+                {
+                    if (charMatrix[row, col] == charMatrix[row, col + 1] &&
+                        charMatrix[row + 1, col] == charMatrix[row + 1, col + 1] && 
+                        charMatrix[row,col]==charMatrix[row+1,col])
+
+                    {
+                        squareCount++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(squareCount);
+
+        }
+    }
+}
---
+++ CsharpAdvanced/MultidimensionalArrays/MultidimensionalArrays-Exercise/demoSquareSum/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace demoSquareSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 2;
+
+            int[] dimetions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = dimetions[0];
+            int cols = dimetions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbersToAdd = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = numbersToAdd[col];
+                }
+            }
+
+
+            int maxSum = 0;
+            int rowToPrint = 0;
+            int colToPrint = 0;
+
+
+            for (int row = 0; row < rows - size + 1; row++)
+            {
+                for (int col = 0; col < cols - size + 1; col++)
+                {
+                    int sum = 0;
+
+                    for (int innerRow = row; innerRow < row + size; innerRow++)
+                    {
+                        for (int innerCol = col; innerCol < col + size; innerCol++)
+                        {
+                            sum += matrix[innerRow, innerCol];
+                        }
+
+                        if (sum > maxSum)
+                        {
+                            maxSum = sum;
+                            rowToPrint = row;
+                            colToPrint = col;
+                        }
+                    }
+                }
+            }
+
+
+            for (int row = rowToPrint; row < rowToPrint + size; row++)
+            {
+                for (int col = colToPrint; col < colToPrint + size; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxSum);
+        }
+    }
+}
