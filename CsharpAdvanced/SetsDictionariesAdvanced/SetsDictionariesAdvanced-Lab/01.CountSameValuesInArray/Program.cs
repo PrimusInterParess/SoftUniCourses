c7dc9469896f551d90d0019302d3cf7126f07923
Dani Borisov <diesonnekind@gmail.com>
2021-02-23 15:24:33 +0200

    'Add first two exercises from SetsAndDictionaries Lab'

---
+++ CsharpAdvanced/SetsDictionariesAdvanced/SetsDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> doubleDictionary = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!doubleDictionary.ContainsKey(numbers[i]))
+                {
+                    doubleDictionary.Add(numbers[i],0);
+                }
+
+                doubleDictionary[numbers[i]]++;
+            }
+
+            foreach (var pair in doubleDictionary)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
---
+++ CsharpAdvanced/SetsDictionariesAdvanced/SetsDictionariesAdvanced-Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentsListDictionary = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+
+                string name = data[0];
+                decimal grade = decimal.Parse(data[1]);
+
+                if (!studentsListDictionary.ContainsKey(name))
+                {
+                    studentsListDictionary.Add(name, new List<decimal>());
+                    studentsListDictionary[name].Add(grade);
+                }
+                else
+                {
+                    studentsListDictionary[name].Add(grade);
+                }
+
+
+            }
+
+            foreach (var pair in studentsListDictionary)
+            {
+                Console.Write($"{pair.Key} -> ");
+
+                foreach (var value in pair.Value)
+                {
+                    Console.Write($"{value:F2} ");
+                }
+
+                Console.WriteLine($"(avg: {pair.Value.Average():F2})");
+            }
+        }
+    }
+}
