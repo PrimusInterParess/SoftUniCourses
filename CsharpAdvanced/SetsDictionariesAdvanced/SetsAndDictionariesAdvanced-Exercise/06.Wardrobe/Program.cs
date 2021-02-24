200b1ba9861017614f63b0d13a4885f9a2eaa918
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-24 18:08:22 +0200

    'Add 06'

--- CsharpAdvanced/SetsDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/06.Wardrobe/Program.cs
+++ CsharpAdvanced/SetsDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/06.Wardrobe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace _06.Wardrobe
@@ -7,11 +9,76 @@ namespace _06.Wardrobe
     {
         static void Main(string[] args)
         {
-            int numberOfClothes = Console.ReadLine();
+            int numberOfClothes = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wordrobe = new Dictionary<string, Dictionary<string, int>>();
+
+          
+
+           
 
             for (int i = 0; i < numberOfClothes; i++)
             {
-                string data = Regex.
+                string[] data = Console.ReadLine().Split(new String[]{" -> ",","},StringSplitOptions.RemoveEmptyEntries);
+
+                string color = data[0];
+
+                var clothes = string.Join(",", data).Substring(color.Length+1).Split(",").ToArray();
+
+
+                if (!wordrobe.ContainsKey(color))
+                {
+                    wordrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (var cloth in clothes)
+                {
+                    string cl = cloth;
+
+                    if (!wordrobe[color].ContainsKey(cl))
+                    {
+                        wordrobe[color].Add(cl, 0);
+
+                    }
+
+                    wordrobe[color][cl]++;
+                }
+
+            }
+
+            string[] find = Console.ReadLine().Split();
+
+            string colorr = find[0];
+            string clot = find[1];
+
+            foreach (var pair in wordrobe)
+            {
+                Console.WriteLine($"{pair.Key} clothes:");
+
+                if (pair.Key == colorr)
+                {
+                    foreach (var pairr in pair.Value)
+                    {
+                        if (pairr.Key == clot)
+                        {
+                            Console.WriteLine($"* {pairr.Key} - {pairr.Value} (found!)");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"* {pairr.Key} - {pairr.Value}");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var pairr in pair.Value)
+                    {
+
+
+                        Console.WriteLine($"* {pairr.Key} - {pairr.Value}");
+
+                    }
+                }
             }
         }
     }
