584d077d36a4df058bdfdf347dd32db9108b8081
Dani Borisov <diesonnekind@gmail.com>
2021-02-25 15:27:59 +0200

    'Add unfinished task'

--- CsharpAdvanced/SetsDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/07.TheV-Logger/Program.cs
+++ CsharpAdvanced/SetsDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/07.TheV-Logger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.TheV_Logger
 {
@@ -6,7 +8,95 @@ namespace _07.TheV_Logger
     {
         static void Main(string[] args)
         {
-           
+            Dictionary<string, Dictionary<string, SortedSet<string>>> vloggersDictionary =
+                new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            string input = string.Empty;
+
+
+
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                string[] data = input.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string command = data[1];
+                string whoToAdd = data[0];
+
+
+                switch (command)
+                {
+                    case "joined":
+                        Joining(vloggersDictionary, whoToAdd);
+                        break;
+                    case "followed":
+                        string whoIsFollowing = data[2];
+                        Following(vloggersDictionary, whoToAdd, whoIsFollowing);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggersDictionary.Count} vloggers in its logs.");
+
+            int count = 1;
+
+            Dictionary<string, Dictionary<string, SortedSet<string>>> sortedDictionary = vloggersDictionary
+                .OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            foreach (KeyValuePair<string,Dictionary<string,SortedSet<string>>> pair in sortedDictionary)
+            {
+                Console.WriteLine($"{count}. {pair.Key} : {pair.Value["followers"].Count} followers, {pair.Value["following"].Count} following");
+
+                if (count == 1)
+                {
+                    foreach (var followers in pair.Value["followers"])
+                    {
+
+                        Console.WriteLine($"*  {followers}");
+                    }
+                }
+                count++;
+
+
+            }
+
+
+        }
+
+        private static void Following(Dictionary<string, Dictionary<string, SortedSet<string>>> vloggersDictionary,
+            string whoToAdd, string whoIsFollowing)
+        {
+            if (vloggersDictionary.ContainsKey(whoIsFollowing) && vloggersDictionary.ContainsKey(whoToAdd))
+            {
+                if (whoIsFollowing != whoToAdd)
+                {
+                    vloggersDictionary[whoIsFollowing]["followers"].Add(whoToAdd);
+
+
+
+                    vloggersDictionary[whoToAdd]["following"].Add(whoIsFollowing);
+
+                }
+
+            }
+
+
+
+        }
+
+        private static void Joining(Dictionary<string, Dictionary<string, SortedSet<string>>> vloggersDictionary, string whoToAdd)
+        {
+            if (!vloggersDictionary.ContainsKey(whoToAdd))
+            {
+                vloggersDictionary.Add(whoToAdd, new Dictionary<string, SortedSet<string>>());
+                vloggersDictionary[whoToAdd].Add("followers", new SortedSet<string>());
+                vloggersDictionary[whoToAdd].Add("following", new SortedSet<string>());
+            }
+
+
+
         }
     }
 }
---
+++ CsharpAdvanced/SetsDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/08.Ranking/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contestDictionary = new Dictionary<string, string>();
+
+            Dictionary<string, Dictionary<string, int>> userData = new Dictionary<string, Dictionary<string, int>>();
+
+            ContestLoading(contestDictionary);
+
+            string input = String.Empty;
+
+            bool isValid = true;
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] data = input.Split("=>", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                isValid = ChecksIfContestIsValid(contestDictionary, data);
+
+                if (isValid)
+                {
+                    AddDataToUserData(data, userData);
+                }
+
+            }
+
+            KeyValuePair<string, Dictionary<string, int>> first = userData.OrderByDescending(x => x.Value.Values.Sum())
+                .First();
+
+            Console.WriteLine($"Best candidate is {first.Key} with total {first.Value.Values.Sum()} points.");
+
+            Console.WriteLine("Ranking");
+
+        }
+
+        private static void AddDataToUserData(string[] data, Dictionary<string, Dictionary<string, int>> userData)
+        {
+            string contest = data[0];
+            string password = data[1];
+            string userName = data[2];
+            int points = int.Parse(data[3]);
+
+            if (!userData.ContainsKey(userName))
+            {
+                userData.Add(userName, new Dictionary<string, int>());
+                userData[userName].Add(contest, 0);
+            }
+            else
+            {
+                if (!userData[userName].ContainsKey(contest))
+                {
+                    userData[userName].Add(contest, 0);
+
+                }
+
+            }
+
+            if (userData[userName].ContainsKey(contest) && userData[userName][contest] < points)
+            {
+                userData[userName][contest] = points;
+            }
+        }
+
+        private static bool ChecksIfContestIsValid(Dictionary<string, string> contestDictionary, string[] data)
+        {
+
+            string contest = data[0];
+            string password = data[1];
+
+            if (contestDictionary.ContainsKey(contest) && contestDictionary[contest] == password)
+            {
+                return true;
+
+            }
+
+            return false;
+
+
+        }
+
+        private static void ContestLoading(Dictionary<string, string> contestDictionary)
+        {
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] contestData = input.Split(":", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string password = contestData[1];
+                string contestName = contestData[0];
+
+                contestDictionary.Add(contestName, password); //?
+
+            }
+        }
+    }
+}
