0c912bd8c8d2ec0da79369cc5a378aea099a6846
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-26 02:13:07 +0200

    Add 01/02 exam problems tomoro more

---
+++ CsharpFundamentals/MidExmsPrep/ProgrammingFundamentalsMidExamRetake6August2019/01BlackFlag/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01BlackFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfPlunder = int.Parse(Console.ReadLine());
+
+            int dailyPlunder = int.Parse(Console.ReadLine());
+
+            int expectedPlunder = int.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            int dayCounter = 1;
+
+            while (daysOfPlunder != 0)
+            {
+                daysOfPlunder--;
+
+                totalPlunder += dailyPlunder;
+
+                if (dayCounter % 3 == 0)
+                {
+                    totalPlunder += (dailyPlunder * 1.5) - dailyPlunder;
+                }
+
+                if (dayCounter % 5 == 0)
+                {
+                    totalPlunder *= 0.70;
+                }
+
+                dayCounter++;
+
+            }
+            
+            if (totalPlunder < expectedPlunder)
+            {
+                double result = (totalPlunder / expectedPlunder) * 100;
+
+                Console.WriteLine($"Collected only {result:F2}% of the plunder.");
+            }
+            else
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+
+        }
+    }
+}
---
+++ CsharpFundamentals/MidExmsPrep/ProgrammingFundamentalsMidExamRetake6August2019/02TreasureHunt/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] command = input.Split();
+
+                string action = command[0];
+
+                switch (action)
+                {
+                    case "Loot":
+                        string[] loot = command.Skip(1).ToArray();
+                        for (int i = 0; i < loot.Length; i++)
+                        {
+                            if (!data.Contains(loot[i]))
+                            {
+                                data.Insert(0, loot[i]);
+                            }
+                        }
+                        break;
+                    case "Drop":
+                        int index = int.Parse(command[1]);
+
+                        if (!(index < 0 || index > data.Count)) //?
+                        {
+                            string toPick = data[index];
+
+                            data.RemoveAt(index);
+
+                            data.Add(toPick);
+                        }
+                        break;
+
+                    case "Steal":
+                        int count = int.Parse(command[1]);
+
+                        if (count > data.Count)
+                        {
+                            List<string> stolen = data.ToList();
+
+                            Console.WriteLine(string.Join(", ", stolen));
+
+                            data.Clear();
+                        }
+                        else
+                        {
+                            List<string> stolen = data.GetRange(data.Count - count, count);
+                            data.RemoveRange(data.Count - count, count);
+                            Console.WriteLine(string.Join(", ", stolen));
+                        }
+                        break;
+                }
+
+
+            }
+
+            
+
+
+            
+
+            if (data.Count == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+            else
+            {
+                double sum = 0;
+
+                for (int i = 0; i < data.Count; i++)
+                {
+                    sum += data[i].Length;
+                }
+
+                double total = sum / data.Count;
+
+                Console.WriteLine($"Average treasure gain: {total:f2} pirate credits.");
+            }
+        }
+    }
+}
