062e6804c2e5affb78937856fd416a13278daa6e
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-02-27 11:51:30 +0200

    'MidExamPassed'

---
+++ CsharpFundamentals/MidExam/ProgrammingFundamentalsMidExam-27 February2021/01.Problem/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _01.Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int neededAmountExperience = int.Parse(Console.ReadLine());
+
+            int countBattles = int.Parse(Console.ReadLine());
+
+            double totalExperiencedGained = 0;
+
+            bool experienceGainded = false;
+
+            int index = -1;
+
+            for (int i = 1; i <=countBattles; i++)
+            {
+                double experienceGained = double.Parse(Console.ReadLine());
+
+                if (i % 3 == 0)
+                {
+                    experienceGained *= 1.15;
+                }
+
+                if (i % 5 == 0)
+                {
+                    experienceGained *= 0.90;
+                }
+
+                totalExperiencedGained += experienceGained;
+
+                index = i;
+
+                if (totalExperiencedGained >= neededAmountExperience)
+                {
+                    experienceGainded = true;
+                    break;
+                }
+
+
+
+            }
+
+            
+
+            if (experienceGainded)
+            {
+                Console.WriteLine($"Player successfully collected his needed experience for {index} battles.");
+            }
+            else
+            {
+                double neededExperience = neededAmountExperience - totalExperiencedGained;
+
+                Console.WriteLine($"Player was not able to collect the needed experience, {neededExperience:f2} more needed.");
+            }
+
+
+
+        }
+    }
+}
---
+++ CsharpFundamentals/MidExam/ProgrammingFundamentalsMidExam-27 February2021/02.Problem/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string input = String.Empty;
+
+            int blackListedNames = 0;
+
+            int countLostNames = 0;
+
+            while ((input = Console.ReadLine()) != "Report")
+            {
+                string[] command = input.Split();
+
+                switch (command[0])
+                {
+                    case "Blacklist":
+                        bool found = false;
+                        int indd = -1;
+
+                        for (int i = 0; i < names.Length; i++)
+                        {
+                            if (names[i] == command[1])
+                            {
+                                indd = i;
+                                found = true;
+                                blackListedNames++;
+                                break;
+                            }
+                            
+                        }
+
+                        if (found)
+                        {
+                            Console.WriteLine($"{names[indd]} was blacklisted.");
+                            names[indd] = "Blacklisted";
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{command[1]} was not found.");
+                        }
+                        break;
+                    case "Error":
+                        int index = int.Parse(command[1]);
+
+                        if (names[index] != "Blacklisted" &&  names[index] != "Lost")
+                        {
+
+                            Console.WriteLine($"{names[index]} was lost due to an error.");
+                            names[index] = "Lost";
+
+                            countLostNames++;
+                        }
+                        break;
+                    case "Change":
+                        int ind = int.Parse(command[1]);
+                        string newName = command[2];
+
+
+                        if (ind >= 0 && ind < names.Length)
+                        {
+                            Console.WriteLine($"{names[ind]} changed his username to {newName}.");
+
+                            names[ind] = newName;
+                        }
+
+                        break;
+                        ;
+
+                }
+
+              
+
+            }
+
+            Console.WriteLine($"Blacklisted names: {blackListedNames} ");
+            Console.WriteLine($"Lost names: {countLostNames}");
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+    }
+}
