15d404962005a5e831fedac4fa41d44ecbc14d16
Dani Borisov <diesonnekind@gmail.com>
2021-03-11 14:51:54 +0200

    Add DefiningClasses

---
+++ CsharpAdvanced/DefiningClasses/DefiningClasses-Exercise/01.DefineClassPerson/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInputData = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string name = personInputData[0];
+
+                int age = int.Parse(personInputData[1]);
+
+                Person person = new Person(name,age);
+
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+        }
+    }
+}
---
+++ CsharpAdvanced/DefiningClasses/DemoDefining/01Car/StartUp.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CarManufacturer;
+
+namespace _01Car
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int carCount = 0;
+
+            int tireIndex = 0;
+
+            Dictionary<int, Tire[]> tiresMap = new Dictionary<int, Tire[]>();
+
+            Dictionary<int, Engine> engines = new Dictionary<int, Engine>();
+
+            string input = string.Empty;
+
+            List<Car> cars = new List<Car>();
+
+            while ((input = Console.ReadLine()) != "No more tires")
+            {
+                string[] tireStrings = input.Split(' ').ToArray();
+
+                Tire[] tires = new Tire[tireStrings.Length / 2];
+
+                int tirecounter = 0;
+
+                for (int i = 0; i < tireStrings.Length - 1; i += 2)
+                {
+                    int year = int.Parse(tireStrings[i]);
+
+                    double pressure = double.Parse(tireStrings[i + 1]);
+
+                    tires[tirecounter] = new Tire(year, pressure);
+
+                    tirecounter++;
+
+                }
+
+                tiresMap[tireIndex] = tires;
+                tireIndex++;
+
+            }
+
+            int engineIndex = 0;
+
+            while ((input = Console.ReadLine()) != "Engines done")
+            {
+                string[] engineInfo = input.Split().ToArray();
+
+                int horsePower = int.Parse(engineInfo[0]);
+
+                double cubicCapacity = double.Parse(engineInfo[1]);
+
+                engines[engineIndex] = new Engine(horsePower, cubicCapacity);
+
+                engineIndex++;
+
+            }
+
+
+            while ((input = Console.ReadLine()) != "Show special")
+            {
+                string[] carData = input.Split().ToArray();
+
+                string make = carData[0];
+
+                string model = carData[1];
+
+                int year = int.Parse(carData[2]);
+
+                double fuelQuantity = double.Parse(carData[3]);
+
+                double fuelCapacity = double.Parse(carData[4]);
+
+                int engIndex = int.Parse(carData[5]);
+
+                Engine engine = engines[engIndex];
+
+                int trIndex = int.Parse(carData[6]);
+
+                List<Tire> tires = tiresMap[trIndex].ToList();
+
+                cars.Add(new Car(make, model, year, fuelQuantity, fuelCapacity, engine, tires));
+            }
+
+
+            foreach (var car in cars)
+            {
+                if (car.Year >= 2017 &&
+                    car.Engine.HorsePower > 330 &&
+                    car.Tires.Select(tire => tire.Pressure).Sum() >= 9 &&
+                    car.Tires.Select(tire => tire.Pressure).Sum() <= 10)
+                { 
+                   car.Drive(20);
+
+                    Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nHorsePowers: {car.Engine.HorsePower}\nFuelQuantity: {car.FuelQuantity}");
+                }
+
+            }
+        }
+    }
+}
