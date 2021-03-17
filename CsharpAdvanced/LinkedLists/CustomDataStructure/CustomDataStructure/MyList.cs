2ef98b3e3539f170a04a0f8f323976a489dfa790
Dani Borisov <diesonnekind@gmail.com>
2021-03-17 13:11:23 +0200

    Add LinkedLists Workshop

---
+++ CsharpAdvanced/LinkedLists/CustomDataStructure/CustomDataStructure/MyList.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDataStructure
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private T[] data;
+
+        private int capacity;
+
+        public MyList()
+        : this(4)
+        {
+        }
+
+        public MyList(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new T[capacity];
+        }
+
+        public int Count { get; private set; }
+
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+
+                return this.data[index];
+            }
+            set
+            {
+
+                this.ValidateIndex(index);
+
+                this.data[index] = value;
+
+            }
+        }
+
+        public void Insert(int index, T element)
+        {
+            this.ValidateIndex(index);
+
+            this.CheckIfResizeIsNeeded();
+
+            for (int i = this.Count - 1; i >= index; i--)
+            {
+                this.data[i + 1] = this.data[i];
+            }
+
+            this.data[index] = element;
+
+            this.Count++;
+        }
+
+        public T RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            var result = this.data[index];
+
+            for (int i = index + 1; i < this.Count; i++)
+            {
+                this.data[i - 1] = this.data[i];
+            }
+
+            this.Count--;
+
+            return result;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            this.ValidateIndex(firstIndex);
+            this.ValidateIndex(secondIndex);
+
+            T firstValue = this.data[firstIndex];
+            data[firstIndex] = this.data[secondIndex];
+            data[secondIndex] = firstValue;
+
+
+        }
+
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.data[i].Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Add(T number)
+        {
+            this.CheckIfResizeIsNeeded();
+
+            this.data[this.Count] = number;
+
+            this.Count++;
+        }
+        public void Clear()
+        {
+            this.Count = 0;
+            this.data = new T[this.capacity];
+        }
+
+        private void Resize()
+        {
+            var newCapacity = this.data.Length * 2;
+
+            var newData = new T[newCapacity];
+
+            for (int i = 0; i < this.data.Length; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            this.data = newData;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                var massage = this.Count == 0
+                    ? "The list is empty" :
+                    $"The list has {this.Count} elements and it is zero-based";
+
+                throw new Exception($"Index out of range .{massage}");
+            }
+        }
+
+        private void CheckIfResizeIsNeeded()
+
+        {
+            if (Count == this.data.Length)
+            {
+                this.Resize();
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+            => (IEnumerator<T>)this.data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
---
+++ CsharpAdvanced/LinkedLists/CustomDataStructure/CustomStack/MyStack.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class MyStack<T>
+    {
+        private T[] data;
+
+        private int capacity;
+
+        public MyStack()
+            : this(4)
+        {
+        }
+
+        public MyStack(int capacity)
+
+        {
+            this.capacity = capacity;
+            this.data = new T[capacity];
+        }
+
+        public int Count { get; private set; }
+
+
+
+        public void Push(T element)
+        {
+            if (this.Count == this.data.Length)
+            {
+                Resize();
+            }
+
+            this.data[this.Count] = element;
+
+            Count++;
+        }
+
+        private void Resize()
+        {
+            T[] newStack = new T[data.Length * 2];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                newStack[i] = data[i];
+            }
+
+            data = newStack;
+        }
+        public T Peek()
+        {
+            this.ValidateStack();
+
+            var result = this.data[this.Count - 1];
+
+            return result;
+        }
+
+        public T Pop()
+        {
+            ValidateStack();
+
+            var result = this.data[this.Count - 1];
+
+            this.Count--;
+
+            return result;
+
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            for (int i = this.Count-1; i >=0; i--)
+            {
+                action(this.data[i]);
+            }
+        }
+
+
+        public void Crear()
+        {
+
+            this.data = new T[this.capacity];
+
+            this.Count = 0;
+        }
+
+
+        private void ValidateStack()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+        }
+
+
+
+    }
+}
