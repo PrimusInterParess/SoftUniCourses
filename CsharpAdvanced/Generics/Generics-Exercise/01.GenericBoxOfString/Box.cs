aa55df6124baaed060968785d44dc2aa03ed3e4a
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-03-21 16:25:58 +0200

    Add

---
+++ CsharpAdvanced/Generics/Generics-Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T> where T : IComparable
+    {
+
+        public Box(List<T> list)
+        {
+            this.TList = list;
+        }
+
+        public List<T> TList { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.TList)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public void Swap(int firstP, int secondP)
+        {
+
+            var first = this.TList[firstP];
+
+            this.TList[firstP] = this. TList[secondP];
+
+            this.TList[secondP] = first;
+
+        }
+
+        public int CountGreaterElements( T elementToCompare)
+        {
+            int counter = 0;
+
+            foreach (var element in TList)
+            {
+                if (elementToCompare.CompareTo(element) < 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ CsharpAdvanced/Generics/Generics-Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> strings = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                strings.Add(input);
+            }
+
+            double toCompare = double.Parse(Console.ReadLine());
+
+
+            Box<double> strigBox = new Box<double>(strings);
+
+            Console.WriteLine(strigBox.CountGreaterElements(toCompare));
+          
+
+           
+
+
+
+
+
+
+        }
+    }
+}
