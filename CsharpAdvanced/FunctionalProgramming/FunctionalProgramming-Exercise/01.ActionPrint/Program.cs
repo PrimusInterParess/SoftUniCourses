7424b53c4cf02d8409741826e81874e0ab8f7f8a
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-03-21 14:31:22 +0200

    Add

---
+++ CsharpAdvanced/FunctionalProgramming/FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string[]> print =  PrintNames;
+
+            print(names);
+
+        }
+
+        static void PrintNames(string[] names)
+        {
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
