ebb65fdaf44d3a50baa31d5ed3a1b875656255f2
Dani Borisov <diesonnekind@gmail.com>
2021-04-08 14:54:15 +0300

    add

---
+++ CsharpAdvanced/ExamPrep/CSharpAdvancedExam-22Feb2020/RevoltRecap/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace RevoltRecap
+{
+    class Position
+    {
+        public Position(int row, int col)
+
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public bool isSafe(int rowCount, int colCount)
+        {
+            if (Row < 0 || Col < 0)
+            {
+                return false;
+            }
+
+            if (Row >= rowCount || Col >= colCount)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void CheckOtherSideMovement(int rowCount, int colCount)
+        {
+
+            if (Row < 0)
+            {
+                Row = rowCount - 1;
+            }
+
+            if (Col < 0)
+            {
+                Col = colCount - 1;
+            }
+
+            if (Row >= rowCount)
+            {
+                Row = 0;
+            }
+
+            if (Col >= colCount)
+            {
+                Col = 0;
+            }
+        }
+
+        public static Position GetDirection(string command)
+        {
+            int row = 0;
+            int col = 0;
+
+            if (command == "left")
+            {
+                col = -1;
+            }
+
+            if (command == "right")
+            {
+                col = 1;
+            }
+
+            if (command == "up")
+            {
+                row = -1;
+            }
+
+            if (command == "down")
+            {
+                row = 1;
+            }
+
+            return new Position(row, col);
+
+
+        }
+
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int nSize = int.Parse(Console.ReadLine());
+
+            int nCommands = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[nSize, nSize];
+
+            Position playerP = GetPlayerPositionAndLoadingMatrix(matrix);
+
+
+            if (nCommands > 0)
+            {
+                matrix[playerP.Row, playerP.Col] = '-';
+            }
+
+            for (int i = 0; i < nCommands; i++)
+            {
+                string command = Console.ReadLine();
+
+                MovePlayer(playerP, command, nSize);
+
+                while (matrix[playerP.Row, playerP.Col] == 'B')
+                {
+                    MovePlayer(playerP, command, nSize);
+                }
+
+                while (matrix[playerP.Row, playerP.Col] == 'T')
+                {
+                    Position direction = Position.GetDirection(command);
+
+                    playerP.Row += direction.Row * -1;
+
+                    playerP.Col += direction.Col * -1;
+
+                }
+
+                if (matrix[playerP.Row, playerP.Col]=='F')
+                {
+                    Console.WriteLine("Player won!");
+
+                    matrix[playerP.Row, playerP.Col] = 'f';
+
+                    PrintMatrix(matrix);
+
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("Player lost!");
+
+            matrix[playerP.Row, playerP.Col] = 'f';
+
+            PrintMatrix(matrix);
+
+        }
+
+        private static void MovePlayer(Position playerP, string command, int nSize)
+        {
+            Position movement = Position.GetDirection(command);
+
+            playerP.Row += movement.Row;
+
+            playerP.Col += movement.Col;
+
+            playerP.CheckOtherSideMovement(nSize, nSize);
+
+
+        }
+
+        private static Position GetPlayerPositionAndLoadingMatrix(char[,] matrix)
+        {
+            Position pPositon = null;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string data = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char currChar = data[col];
+
+                    matrix[row, col] = currChar;
+
+                    if (currChar == 'f')
+                    {
+                        pPositon = new Position(row, col);
+
+                    }
+                }
+
+            }
+
+            return pPositon;
+
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+
+                    Console.Write(matrix[row, col]);
+
+
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CsharpAdvanced/ExamPrep/CSharpAdvancedExam-22Feb2020/Snake/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace Snake
+{
+    class Position
+    {
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                int n = int.Parse(Console.ReadLine());
+
+                char[,] matrix = new char[n, n];
+
+                int[] snakePosition = GetPlayerPositionAndLoadingMatrix(matrix);
+
+                int snakeRow = snakePosition[0];
+                int snakeCol = snakePosition[1];
+
+                int fBRow = snakePosition[2];
+                int fBCol = snakePosition[3];
+
+                int sBRow = snakePosition[4];
+                int sBCol = snakePosition[5];
+
+                matrix[snakeRow, snakeCol] = '.';
+
+                int foodCount = 0;
+
+                while (true)
+                {
+
+                    if (foodCount >= 10)
+                    {
+                        Console.WriteLine($"You won! You fed the snake.");
+                        matrix[snakeRow, snakeCol] = 'S';
+                        break;
+
+                    }
+
+                    string command = Console.ReadLine();
+
+
+                    if (command == "up")
+                    {
+                        snakeRow--;
+                    }
+                    if (command == "down")
+                    {
+                        snakeRow++;
+                    }
+                    if (command == "left")
+                    {
+                        snakeCol--;
+                    }
+                    if (command == "right")
+                    {
+                        snakeCol++;
+                    }
+
+                    if (!isSafe(matrix, snakeRow, snakeCol))
+                    {
+                        Console.WriteLine($"Game over!");
+                        break;
+                    }
+
+                    if (matrix[snakeRow, snakeCol] == '*')
+                    {
+                        foodCount++;
+                    }
+
+                    if (matrix[snakeRow, snakeCol] == 'B')
+                    {
+                        matrix[snakeRow, snakeCol] = '.';
+
+                        if (fBRow == snakeRow && fBCol == snakeCol)
+                        {
+                            snakeRow = sBRow;
+
+                            snakeCol = sBCol;
+                        }
+                        else
+                        {
+                            snakeRow = fBRow;
+
+                            snakeCol = fBCol;
+                        }
+
+                    }
+
+                    matrix[snakeRow, snakeCol] = '.';
+
+
+                }
+
+                Console.WriteLine($"Food eaten: {foodCount}");
+
+
+                PrintMatrix(matrix);
+            }
+
+            public static bool isSafe(char[,] matrix, int row, int col)
+            {
+                if (row < 0 || col < 0)
+                {
+                    return false;
+                }
+
+                if (row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+
+
+            private static int[] GetPlayerPositionAndLoadingMatrix(char[,] matrix)
+            {
+                int[] sPosition = new int[6];
+
+                sPosition[2] = -1;
+                sPosition[3] = -1;
+                sPosition[4] = -1;
+                sPosition[5] = -1;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    string data = Console.ReadLine();
+
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        char currChar = data[col];
+
+                        matrix[row, col] = currChar;
+
+                        if (currChar == 'S')
+                        {
+                            sPosition[0] = row;
+
+                            sPosition[1] = col;
+
+                        }
+
+                        if (sPosition[2] == -1 && currChar == 'B')
+                        {
+                            sPosition[2] = row;
+                        }
+
+                        if (sPosition[3] == -1 && currChar == 'B')
+                        {
+                            sPosition[3] = col;
+
+                            continue;
+                        }
+
+                        if (sPosition[4] == -1 && currChar == 'B')
+                        {
+                            sPosition[4] = row;
+                        }
+
+                        if (sPosition[5] == -1 && currChar == 'B')
+                        {
+                            sPosition[5] = col;
+                        }
+                    }
+
+                }
+
+                return sPosition;
+
+            }
+
+            static void PrintMatrix(char[,] matrix)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write(matrix[row, col]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
