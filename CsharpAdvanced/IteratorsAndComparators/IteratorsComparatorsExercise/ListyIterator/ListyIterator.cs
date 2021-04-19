67671e2b73c5082259c5c8faef0d68470b50c8cb
Dani Borisov <diesonnekind@gmail.com>
2021-04-19 15:00:29 +0300

    add first three

---
+++ CsharpAdvanced/IteratorsAndComparators/IteratorsComparatorsExercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index;
+
+        private List<T> list;
+
+        public ListyIterator(params T[] data)
+        {
+            this.list = new List<T>(data);
+
+            this.index = 0;
+
+        }
+
+        public void Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid operation!");
+            }
+
+            Console.WriteLine(this.list[this.index]);
+        }
+
+        public bool Move()
+        {
+            bool hasNext = HasNext();
+
+            if (hasNext)
+            {
+                index++;
+            }
+
+            return hasNext;
+        }
+
+        public bool HasNext()
+        {
+            return this.index < this.list.Count - 1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in list)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+        //private class GetCustomEnumerator : IEnumerator<T>
+        //{
+        //    private List<T> data;
+
+        //    private int index;
+
+        //    public GetCustomEnumerator(List<T> inputData)
+        //    {
+        //        Reset();
+
+        //        this.data = new List<T>(inputData);
+        //    }
+
+
+        //    public bool MoveNext()
+        //    {
+        //        return ++this.index < this.data.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        this.index = -1;
+        //    }
+
+        //    public T Current => this.data[index];
+
+        //    object IEnumerator.Current => Current;
+
+        //    public void Dispose()
+        //    {
+
+        //    }
+        //}
+
+    }
+
+
+}
---
+++ CsharpAdvanced/IteratorsAndComparators/IteratorsComparatorsExercise/ListyIterator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            ListyIterator<string> listy = new ListyIterator<string>(data);
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string command = input;
+
+                if (command == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+
+                if (command == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+
+                if (command == "Print")
+                {
+                    try
+                    {
+                        listy.Print();
+                    }
+                    catch (InvalidOperationException exeption)
+                    {
+                        Console.WriteLine(exeption.Message);
+                    }
+                }
+
+                if (command == "PrintAll")
+                {
+
+
+                    foreach (string item in listy)
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                    Console.WriteLine();
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
