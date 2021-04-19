97a1fecbcf28ce34671d629c9afc954e5fc4b61a
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-04-19 22:39:35 +0300

    add solved "03Stack"

---
+++ CsharpAdvanced/IteratorsAndComparators/IteratorsComparatorsExercise/Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> stackList;
+
+        public Stack(params T[] data)
+        {
+            this.stackList = new List<T>(data.Reverse());
+        }
+
+        public T Pop()
+        {
+            if (stackList.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+
+            }
+
+            var toPop = stackList[0];
+
+            stackList.RemoveAt(0);
+
+            return toPop;
+
+        }
+
+        public void Push(T item)
+        {
+            stackList.Insert(0, item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.stackList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CsharpAdvanced/IteratorsAndComparators/IteratorsComparatorsExercise/Stack/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine()
+                .Split(new char[]{' ',',',' '},StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            Stack<string> custom = new Stack<string>(data);
+
+
+            string command = Console.ReadLine();
+
+            while (command!="END")
+            {
+                string[] action = command.Split();
+
+                if (action[0]=="Pop")
+                {
+                    try
+                    {
+                        custom.Pop();
+                    }
+                    catch (InvalidOperationException exeException)
+                    {
+                        Console.WriteLine(exeException.Message);
+
+                    }
+                }
+
+                if (action[0]=="Push")
+                {
+                    custom.Push(action[1]);
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+
+            foreach (var item in custom)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in custom)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+    }
+}
