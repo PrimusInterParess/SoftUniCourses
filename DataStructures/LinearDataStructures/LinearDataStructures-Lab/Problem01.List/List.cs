6a1108c02027fe8859c1fb0e8a9ab19708d4e637
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-05-12 22:16:05 +0300

    add

---
+++ DataStructures/LinearDataStructures/LinearDataStructures-Lab/Problem01.List/List.cs
@@ -0,0 +1,151 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] _items;
+
+
+
+        public List(int capacity = DEFAULT_CAPACITY)
+        {
+            if (capacity <= 0)
+            {
+                throw new AggregateException(nameof(capacity));
+            }
+
+            this._items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this._items[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            this.GrowIfNecessery();
+            this._items[Count++] = item;
+        }
+
+
+
+
+        public bool Contains(T item)
+        {
+            return this.IndexOf(item) != -1;
+        }
+
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+            this.GrowIfNecessery();
+
+            for (int i = Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+            this.Count++;
+
+
+        }
+
+
+
+        public bool Remove(T item)
+        {
+            int indexOf = this.IndexOf(item);
+
+            if (indexOf == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(indexOf);
+
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void GrowIfNecessery()
+        {
+            if (this.Count == this._items.Length)
+            {
+                this._items = this.Grow();
+            }
+        }
+
+        private T[] Grow()
+        {
+            T[] newArr = new T[Count * 2];
+            for (int i = 0; i < Count; i++)
+            {
+                newArr[i] = this._items[i];
+            }
+
+            return newArr;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+        }
+    }
+}
