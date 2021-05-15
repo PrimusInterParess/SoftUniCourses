1da4f0a59048628ece27fd6c16b820c840615a92
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-05-16 01:28:40 +0300

    Add

---
+++ CsharpOOP/AbstractionAndInterfaces/InterfacesAbstraction-Exercise/InterfacesAbstraction-Exercise/MilitaryElite/Contracts/IComando.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MilitaryElite.Contracts
+{
+    public interface IComando:ISpecialisedSoldier
+    {
+        IReadOnlyCollection<IMission> Missions { get; }
+
+        void AddMission(IMission mission);
+    }
+}
