4762199cc1426e161980522135e8df5372e13b33
Dani Borisov <diesonnekind@gmail.com>
2021-08-27 15:27:53 +0300

    slowly but shurely

--- CsharpDatabase/MSSQL/07Relations/01Lab/Relations.sql
+++ CsharpDatabase/MSSQL/07Relations/01Lab/Relations.sql
@@ -3,12 +3,42 @@ use CoursesTest
 
 CREATE TABLE Students
 (
-Id INT PRIMARY KEY IDENTITY NOT NULL,
-[Name] NVARCHAR(100) NOT NULL,
-FacultyNumber char(6) NOT NULL,
-Photo varbinary(max),
-DateOfEnlistment date,
-)
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(100) NOT NULL,
+	FacultyNumber char(6) NOT NULL UNIQUE,
+	Photo varbinary(max),
+	DateOfEnlistment date,
+) 
 
 ALTER TABLE Students
 ADD Courses nvarchar(500)
+
+create table Towns
+(
+Id int primary key identity,
+[Name] varchar(50) unique,
+
+)
+
+insert into Towns(Name)
+values
+('Sofia'),
+('Plovdiv'),
+('Ruse'),
+('Varna'),
+('Stara Zagora')
+
+create table Course
+(
+Id int primary key identity,
+[Name] varchar(100) Not null,
+TownId int not null foreign key references Towns(Id)
+)
+
+insert into Course([Name],TownId)
+values
+('C#',1),
+('Python',2),
+('HTML',3),
+('PHP',4),
+('CSS',1)
