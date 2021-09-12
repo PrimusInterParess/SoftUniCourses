fccb21099053cd984ddac44cf6f2256ff1c9ef61
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2021-09-12 11:08:36 +0300

    rolling in the deep

---
+++ AlgorithmsCsharp/01AlgorithmsFundamentals/03RecursionBacktracking/01.RecursiveArraySum/05PathInLabyrinth/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05PathInLabyrinth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+
+            char[,] matrix = GenerateMatrix(row, col);
+            List<char> directions = new List<char>();
+
+            FindingPaths(matrix, 0,0, directions, '\0');
+        }
+
+        private static void FindingPaths(char[,] matrix, int row, int col, List<char> directions, char direction)
+        {
+            if (IsOutside(matrix, row, col) ||
+                IsWall(matrix,row,col) ||
+                IsVisited(matrix,row,col))
+            {
+                return;
+                
+            }
+
+            directions.Add(direction);
+
+
+            if (IsPath(matrix,row,col))
+            {
+               // voidPrintLabirint(matrix);
+
+               Console.WriteLine(string.Join("",directions));
+                directions.RemoveAt(directions.Count - 1);
+
+                return;
+            }
+            matrix[row, col] = 'v';
+
+            FindingPaths(matrix, row, col + 1,directions, 'R');
+            FindingPaths(matrix, row, col - 1, directions, 'L');
+            FindingPaths(matrix, row - 1, col,directions, 'U');
+            FindingPaths(matrix, row + 1, col,directions, 'D');
+
+            directions.RemoveAt(directions.Count - 1);
+            matrix[row, col] = '-';
+        }
+
+        private static bool IsPath(char[,] matrix, int row, int col)
+        {
+            return matrix[row, col] == 'e';
+        }
+
+        private static bool IsVisited(char[,] matrix, int row, int col)
+        {
+            return matrix[row, col] == 'v';
+        }
+
+        private static bool IsWall(char[,] matrix, int row, int col)
+        {
+            return matrix[row, col] == '*';
+        }
+
+        private static bool IsOutside(char[,] matrix, int row, int col)
+        {
+          return  row >= matrix.GetLength(0) || 
+                  row < 0 || 
+                  col < 0 || 
+                  col >= matrix.GetLength(1);
+
+
+        }
+
+        private static char[,] GenerateMatrix(int row, int col)
+        {
+            char[,] toReturn = new char[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                string rowstring = Console.ReadLine();
+
+                for (int j = 0; j < col; j++)
+                {
+                    toReturn[i, j] = rowstring[j];
+                }
+            }
+
+            return toReturn;
+        }
+
+        private static void  voidPrintLabirint(char[,] matrix)
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+           
+        }
+    }
+}
---
+++ AlgorithmsCsharp/01AlgorithmsFundamentals/03RecursionBacktracking/01.RecursiveArraySum/EightQ/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EightQ
+{
+    class Program
+    {
+
+        static HashSet<int> occupiedRows = new HashSet<int>();
+        static HashSet<int> occupiedCols = new HashSet<int>();
+        static HashSet<int> occupiedrightDiagonals = new HashSet<int>();
+        static HashSet<int> occupiedleftDiagonals = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            bool[,] board = new bool[8, 8];
+
+            PlaceQueens(board, 0);
+        }
+
+        private static void PlaceQueens(bool[,] board, int row)
+        {
+            if (row == board.GetLength(0))
+            {
+                PrintBoard(board);
+                return;
+            }
+
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (!IsOccupied(row, col))
+                {
+                    board[row, col] = true;
+                    occupiedRows.Add(row);
+                    occupiedCols.Add(col);
+                    occupiedleftDiagonals.Add(row - col);
+                    occupiedrightDiagonals.Add(row + col);
+
+                    PlaceQueens(board, row + 1);
+
+                    board[row, col] = false;
+                    occupiedRows.Remove(row);
+                    occupiedCols.Remove(col);
+                    occupiedleftDiagonals.Remove(row - col);
+                    occupiedrightDiagonals.Remove( row + col);
+
+                   
+                }
+
+
+            }
+        }
+
+        private static bool IsOccupied(int row, int col)
+        {
+            return occupiedRows.Contains(row) ||
+                   occupiedCols.Contains(col) ||
+                   occupiedleftDiagonals.Contains(row - col) ||
+                   occupiedrightDiagonals.Contains(row + col);
+
+
+        }
+
+        private static void PrintBoard(bool[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i,j])
+                    {
+                        Console.Write($"* ");
+                    }
+                    else
+                    {
+                        Console.Write($"- ");
+                    }
+
+                   
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
