c25405a4f209281d9a562d580f615fd1d9c3b669
Dani Borisov <diesonnekind@gmail.com>
2021-12-17 15:26:41 +0200

    introDynamicProg_Fibonacci

---
+++ C#/C#Algorithms/01AlgorithmsFundamentals/08IntroDynamicProgramming/01Lab/01DynamicProgrammingLab/01Fibonacci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _01Fibonacci
+{
+    class Program
+    {
+        private static Dictionary<int, long> memo;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            memo = new Dictionary<int, long>();
+            Console.WriteLine(GetFibonacci(n));
+
+        }
+
+        private static long GetFibonacci(int fib)
+        {
+
+            if (memo.ContainsKey(fib))
+            {
+                return memo[fib];
+            }
+
+            if (fib <= 2)
+            {
+                return 1;
+            }
+
+
+            var result = GetFibonacci(fib - 1) + GetFibonacci(fib - 2);
+
+            memo[fib] = result;
+
+            return result;
+        }
+    }
+}
