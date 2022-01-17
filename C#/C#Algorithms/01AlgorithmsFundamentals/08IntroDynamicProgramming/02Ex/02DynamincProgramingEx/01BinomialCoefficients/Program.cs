0b6ceb4586be12f233cc7ad2a8dd491f1c11261e
Dani Borisov <diesonnekind@gmail.com>
2022-01-17 14:58:23 +0200

    dynamicPrgrm_SubsetSum

---
+++ C#/C#Algorithms/01AlgorithmsFundamentals/08IntroDynamicProgramming/02Ex/02DynamincProgramingEx/01BinomialCoefficients/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _01BinomialCoefficients
+{
+    class Program
+    {
+
+        private static Dictionary<string, BigInteger> memo;
+
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            memo = new Dictionary<string, BigInteger>();
+
+            BigInteger result = GetBinom(r, c);
+
+            Console.WriteLine(result);
+
+        }
+
+        private static BigInteger GetBinom(int row, int col)
+        {
+            var key = $"{row}-{col}";
+
+            if (memo.ContainsKey(key))
+            {
+                return memo[key];
+            }
+
+            if (row <= 1 || col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var result = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+
+            memo.Add(key,result);
+            
+            return result;
+        }
+    }
+}
---
+++ C#/C#Algorithms/01AlgorithmsFundamentals/08IntroDynamicProgramming/02Ex/02DynamincProgramingEx/03SumwithUnlimitedCoins/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _03SumwithUnlimitedCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbs = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            var targetSum = int.Parse(Console.ReadLine());
+
+            var result = CountSums(numbs, targetSum);
+
+            Console.WriteLine(result);
+        }
+
+        private static int CountSums(int[] numbs, int targetSum)
+        {
+            var sums = new int[targetSum + 1];
+
+            sums[0] = 1;
+
+            foreach (var num in numbs)
+            {
+
+                for (int sum = num; sum <= targetSum; sum++)
+                {
+                    sums[sum] += sums[sum - num];
+
+                }
+
+            }
+
+            return sums[targetSum];
+        }
+    }
+}
