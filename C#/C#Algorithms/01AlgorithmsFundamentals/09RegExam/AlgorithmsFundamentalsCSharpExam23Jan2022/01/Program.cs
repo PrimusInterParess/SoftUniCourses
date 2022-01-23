1fec0ca7d704ea2cedf3a2b14489facea971968a
Dani Borisov <diesonnekind@gmail.com>
2022-01-23 12:45:54 +0200

    algrthmsFundExam_failure

---
+++ C#/C#Algorithms/01AlgorithmsFundamentals/09RegExam/AlgorithmsFundamentalsCSharpExam23Jan2022/01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01BinomialCoefficients
+{
+    class Program
+    {
+
+        private static Dictionary<string, long> memo;
+
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            memo = new Dictionary<string, long>();
+
+            long result = GetBinom(r, c);
+
+            Console.WriteLine(result);
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            var key = $"{row}-{col}";
+
+            if (memo.ContainsKey(key))
+            {
+                return memo[key];
+            }
+
+            if (row == 1 || col == 1 || row==col)
+            {
+                return 1;
+            }
+
+            var result = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+
+            memo.Add(key, result);
+
+            return result;
+        }
+    }
+}
---
+++ C#/C#Algorithms/01AlgorithmsFundamentals/09RegExam/AlgorithmsFundamentalsCSharpExam23Jan2022/03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _03.VariationsWithoutRepetition
+{
+    internal class Program
+    {
+        private static Char[] arr;
+        private static Char[] variations;
+        private static bool[] used;
+        private static int k;
+
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine();
+            arr = elements.ToCharArray();
+            k = elements.Length;
+
+            variations = new Char[k];
+            used = new bool[elements.Length];
+
+            Variations();
+        }
+
+        private static void Variations(int index = 0)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(String.Join("", variations));
+                return;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (used[i] == false)
+                {
+                    used[i] = true;
+                    variations[index] = char.ToUpper(arr[i]);
+                    Variations(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
