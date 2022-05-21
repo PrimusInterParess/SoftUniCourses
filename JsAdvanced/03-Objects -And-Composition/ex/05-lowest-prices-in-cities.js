4624526b248c499b40727d5cc4e49a1d60ec89bf
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-05-21 18:39:09 +0300

    mess

--- JsAdvanced/03-Objects -And-Composition/ex/05-lowest-prices-in-cities.js
+++ JsAdvanced/03-Objects -And-Composition/ex/05-lowest-prices-in-cities.js
@@ -1,20 +1,52 @@
-function findPrice(array) {
-    let parts = array.split(',')
+// function findPrice(array) {
+//     let parts = array.split(',')
 
-    let towns = {}
+//     let towns = {}
 
-    for (const part of parts) {
-        let [townName, product, price] = part.split(' | ');
+//     for (const part of parts) {
+//         let [townName, product, price] = part.split(' | ');
 
-      
+
+//     }
+// }
+
+// findPrice(['Sample Town | Sample Product | 1000',
+//     'Sample Town | Orange | 2',
+//     'Sample Town | Peach | 1',
+//     'Sofia | Orange | 3',
+//     'Sofia | Peach | 2',
+//     'New York | Sample Product | 1000.1',
+//     'New York | Burger | 10']
+// )
+
+function solve(input) {
+    let products = {};
+
+    for (const inputInfo of input) {
+        let [town, product, price] = inputInfo.split(' | ');
+        price = Number(price);
+
+        if (!products.hasOwnProperty(product)) {
+            products[product] = {};
+        }
+
+        products[product][town] = price;
+    }
+
+    for (const product in products) {
+        let townsSorted = Object.entries(products[product]).sort((a, b) => a[1] - b[1]);
+        console.log(`${product} -> ${townsSorted[0][0]} (${townsSorted[0][1]})`);
     }
 }
 
-findPrice(['Sample Town | Sample Product | 1000',
+
+solve(['Sofia City | Audi | 100000', 'Mexico City | Audi | 1000', 'Mexico City | Audi | 100000']);
+
+solve(['Sample Town | Sample Product | 1000',
     'Sample Town | Orange | 2',
     'Sample Town | Peach | 1',
     'Sofia | Orange | 3',
     'Sofia | Peach | 2',
     'New York | Sample Product | 1000.1',
-    'New York | Burger | 10']
-)
+    'New York | Burger | 10'
+]);
