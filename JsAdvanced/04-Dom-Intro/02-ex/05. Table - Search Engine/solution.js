26fdd75e1136cd993aa55c1e857dfcd5e3533632
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-05-22 09:36:08 +0300

    js-dom-intr-ex

--- JsAdvanced/04-Dom-Intro/02-ex/05. Table - Search Engine/solution.js
+++ JsAdvanced/04-Dom-Intro/02-ex/05. Table - Search Engine/solution.js
@@ -1,8 +1,31 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+      function solve() {
+          document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+          function onClick() {
+              let criteria = document.getElementById('searchField').value;
+              let parent = document.querySelectorAll('tbody tr');
 
-   }
-}
+              for (const child of parent) {
+
+                  let color = child.style.backgroundColor;
+
+                  if (child.className == 'select') {
+                      child.classList.remove('select')
+                  }
+              }
+
+              for (const child of parent) {
+                  let children = child.getElementsByTagName('td')
+
+                  for (const elements of children) {
+                      let currentEl = elements.textContent;
+
+                      if (criteria !== ' ' && criteria !== '' && (currentEl === criteria ||
+                              currentEl.includes(criteria))) {
+                          child.classList.add('select')
+                      }
+
+                  }
+              }
+          }
+      }
