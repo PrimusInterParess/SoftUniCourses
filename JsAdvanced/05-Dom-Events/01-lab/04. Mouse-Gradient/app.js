cc21a2357c6d68c48128457d3dfc5350a560440e
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-05-25 23:21:38 +0300

    js-dom-events-lab

--- JsAdvanced/05-Dom-Events/01-lab/04. Mouse-Gradient/app.js
+++ JsAdvanced/05-Dom-Events/01-lab/04. Mouse-Gradient/app.js
@@ -2,11 +2,9 @@ function attachGradientEvents() {
     let hoverElement = document.getElementById('gradient');
 
     hoverElement.addEventListener('mousemove', function(e) {
-
         let power = e.offsetX / (e.target.clientWidth - 1);
         power = Math.floor(power * 100);
         document.getElementById('result').textContent = power + "%";
-
     })
 
 }
--- JsAdvanced/05-Dom-Events/01-lab/05. Highlight-Active/app.js
+++ JsAdvanced/05-Dom-Events/01-lab/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    console.log('TODO:...');
+
+    Array.from(document.querySelectorAll('input')).forEach(e => {
+        e.addEventListener('focus', (e) => {
+            e.target.parentNode.className = 'focused';
+        })
+        e.addEventListener('blur', (e) => {
+            e.target.parentNode.className = '';
+        });
+    });
+
 }
