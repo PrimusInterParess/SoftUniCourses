5e2452607de46881434fcaed8ab96420c12f9e6d
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-05-26 23:53:35 +0300

    js-dom-events-lab

--- JsAdvanced/05-Dom-Events/01-lab/02. Delete-From-Table/app.js
+++ JsAdvanced/05-Dom-Events/01-lab/02. Delete-From-Table/app.js
@@ -1,20 +1,31 @@
 function deleteByEmail() {
-    let inputSearchElement = document.querySelector('input[name="email"]');
-    let tdEmailElements = document.querySelectorAll('tbody tr td:nth-child(2)');
-    let resultElement= document.querySelector('div[id="result"]');
-    let result ='';
 
-    let deletedLiElement = Array.from(tdEmailElements).forEach(e => {
-        if (e.textContent == inputSearchElement.value) {
-            e.parentElement.remove();
-            result='Deleted';
-            return;
-        };
-    });
-    
-   if(result!='Deleted'){
-    result='Not found.'
-   }
-    resultElement.textContent= result;
+    const input = document.querySelector('input[name="email"]');
+
+    const rows = Array.from(document.querySelector('tbody').children)
+        .filter(r => r.children[1].textContent == input.value);
+
+    rows.forEach(r => r.remove());
+
+    console.log(rows.length);
+    document.getElementById('result').textContent = rows.length > 0 ? 'Deleted' : 'Not found';
+
+    //     let inputSearchElement = document.querySelector('input[name="email"]');
+    //     let tdEmailElements = document.querySelectorAll('tbody tr td:nth-child(2)');
+    //     let resultElement= document.querySelector('div[id="result"]');
+    //     let result ='';
+
+    //     let deletedLiElement = Array.from(tdEmailElements).forEach(e => {
+    //         if (e.textContent == inputSearchElement.value) {
+    //             e.parentElement.remove();
+    //             result='Deleted';
+    //             return;
+    //         };
+    //     });
+
+    //    if(result!='Deleted'){
+    //     result='Not found.'
+    //    }
+    //     resultElement.textContent= result;
 
 }
--- JsAdvanced/05-Dom-Events/01-lab/06. Dynamic-Validation/app.js
+++ JsAdvanced/05-Dom-Events/01-lab/06. Dynamic-Validation/app.js
@@ -1,20 +1,15 @@
 function validate() {
-    document.getElementById('email').addEventListener('keypress',function(e){
+    document.getElementById('email').addEventListener('change', function(e) {
 
         var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         let email = e.currentTarget.value;
 
-     if(!email.match(validRegex)){
+        if (!email.match(validRegex)) {
 
-        e.currentTarget.className= 'error';
-     }
-     else{
-        e.currentTarget.className= '';
-     }
+            e.currentTarget.className = 'error';
+        } else {
+            e.currentTarget.className = '';
+        }
 
-        // if(){
-        //    
-        // }
-    
     })
 }
