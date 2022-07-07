2193ae3872f4241de655bf032d36512acd7b4a44
Dani Borisov <diesonnekind@gmail.com>
2022-07-07 14:49:52 +0300

    js-applicaitons-async-programming

--- JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/01.XHR/scripts/app.js
+++ JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/01.XHR/scripts/app.js
@@ -1,3 +1,12 @@
-function loadRepos() {
-   console.log("TODO...");
+async function loadRepos() {
+
+   let result = document.getElementById('res');
+
+   let res = await fetch('https://api.github.com/users/testnakov/repos');
+
+   let data = await res.json();
+
+   result.textContent = JSON.stringify(data);
+
+   console.log(data);
 }
--- JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/02.Github-Repos/scripts/app.js
+++ JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/02.Github-Repos/scripts/app.js
@@ -1,7 +1,17 @@
-function loadRepos() {
+async function loadRepos() {
     let inputField = document.getElementById('username');
 
-    fetch(`https://api.github.com/users/${inputField.value}/repos`)
-        .then(res => res.json())
-        .then(data => console.log(data.map(({ full_name, html_url }) => ({ full_name, html_url }))));
+    let repos = document.getElementById('repos');
+
+    let result = await fetch(`https://api.github.com/users/${inputField.value}/repos`)
+       // .then(res => res.json())
+    // .then(data => (data.map(({ full_name, html_url }) => ({ full_name, html_url }))));
+
+    console.log(result);
+
+
+    // for (const repo of result) {
+
+    //     console.log(repo.full_name, repo.html_url);
+    // }
 }
