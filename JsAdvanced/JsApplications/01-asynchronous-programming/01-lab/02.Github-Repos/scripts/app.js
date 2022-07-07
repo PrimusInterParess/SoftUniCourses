5906e9398d0c71e42f87f8b947a6b86b3132419c
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-07-08 00:16:56 +0300

    js-applications-async-programming-lab

--- JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/02.Github-Repos/scripts/app.js
+++ JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/02.Github-Repos/scripts/app.js
@@ -3,15 +3,29 @@ async function loadRepos() {
 
     let repos = document.getElementById('repos');
 
-    let result = await fetch(`https://api.github.com/users/${inputField.value}/repos`)
-       // .then(res => res.json())
-    // .then(data => (data.map(({ full_name, html_url }) => ({ full_name, html_url }))));
+    try {
+        let response = await fetch(`https://api.github.com/users/${inputField.value}/repos`);
 
-    console.log(result);
+        if (response.ok == false) {
+            throw new Error(`${response.status} "${'Not Found'}"`);
+        }
 
+        let data = await response.json();
 
-    // for (const repo of result) {
+        let extractedRepoInfo = data.map(({ full_name, html_url }) => ({ full_name, html_url }));
 
-    //     console.log(repo.full_name, repo.html_url);
-    // }
+        for (const repo of extractedRepoInfo) {
+
+            repos.innerHTML = '';
+            let liEl = document.createElement('li');
+            let aEl = document.createElement('a');
+            aEl.href = repo.html_url;
+            aEl.textContent = repo.full_name;
+            liEl.appendChild(aEl);
+            repos.appendChild(liEl);
+
+        }
+    } catch (err) {
+        repos.textContent = err.message;
+    }
 }
--- JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/03.Gighub-Commits/app.js
+++ JsAdvanced/JsApplications/01-asynchronous-programming/01-lab/03.Gighub-Commits/app.js
@@ -1,4 +1,38 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    let usernameInputField = document.getElementById('username');
+
+    let repoInputField = document.getElementById('repo');
+
+    let ulElement = document.getElementById('commits');
+
+    try {
+        let response = await fetch(`https://api.github.com/repos/${usernameInputField.value}/${repoInputField.value}/commits`);
+
+        if (response.ok == false) {
+            throw new Error(`${response.status} (Not Found)`);
+        }
+        ulElement.innerHTML = '';
+
+        let data = await response.json();
+
+        for (const { commit }
+            of data) {
+
+            let liEl = document.createElement('li');
+            liEl.textContent = `${commit.author.name}: ${commit.message}`
+            ulElement.appendChild(liEl);
+        }
+
+        //let extractedCommitData = data.map(({ commit_author_name }) => ({ commit_author_name }))
+
+
+        for (const iterator of object) {
+            //"<commit.author.name>: <commit.message>" 
+        }
+    } catch (err) {
+
+        let liEl = document.createElement('li');
+        liEl.textContent = err.message;
+        ulElement.appendChild(liEl);
+    }
 }
