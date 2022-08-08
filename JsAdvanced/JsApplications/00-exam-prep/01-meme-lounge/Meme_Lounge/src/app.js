854fdf639738a6c9d88495a228e8a44e1e8f73ef
Dani Borisov <diesonnekind@gmail.com>
2022-08-08 15:49:50 +0300

    js-application-exam-prep

---
+++ JsAdvanced/JsApplications/00-exam-prep/01-meme-lounge/Meme_Lounge/src/app.js
@@ -0,0 +1,30 @@
+import { page, render } from './lib.js';
+import { catalogView } from './views/catalog.js';
+import { homeView } from './views/home.js';
+
+const main = document.querySelector('main');
+
+page(decorateContext);
+page('/', homeView);
+page('/memes', catalogView);
+page('/memes/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+page('/profile', () => console.log('profile'));
+page('/fakOf', () => console.log('fakOf'));
+
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+
+    next();
+}
+
+function renderMain(temlateResult) {
+
+    render(temlateResult, main)
+}
