7bed08ae49b71f0454801ad551554289f01c9755
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2023-04-19 23:23:16 +0300

    route-workshop-auth-add

--- SoftUniCourses/ReactJs/workshop-routing/games/src/App.js
+++ SoftUniCourses/ReactJs/workshop-routing/games/src/App.js
@@ -5,6 +5,7 @@ import { Register } from "./components/Register/Register";
 import { Create } from "./components/Create/Create";
 import { Catalog } from "./components/Catalog/Catalog";
 import { Details } from "./components/Details/Details";
+import { AuthContext } from "./contexts/AuthContext";
 
 import * as gameService from './services/gameService';
 
@@ -14,6 +15,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 function App() {
 
     const [games, setGames] = useState([]);
+    const [auth, setAut] = useState({});
     const navigate = useNavigate();
     useEffect(() => {
         gameService.getAll()
@@ -28,20 +30,26 @@ function App() {
         setGames(state => [...state, newGame])
         navigate('/catalog');
     }
+
+    const onLoginSubmit = async (values) => {
+        console.log(values);
+    }
     return (
+        <AuthContext.Provider value={{onLoginSubmit}}>
         <div className="App">
-            <Header />
-            <main id="main-content">
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/catalog" element={<Catalog games={games} />} />
-                    <Route path="/catalog/:gameId" element={<Details games={games} />} />
-                    <Route path="/create" element={<Create onCreateSubmit={onCreateGameSubmit} />} />
-                </Routes>
-            </main>
-        </div>
+                <Header />
+                <main id="main-content">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path="/login" element={<Login/>} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/catalog" element={<Catalog games={games} />} />
+                        <Route path="/catalog/:gameId" element={<Details games={games} />} />
+                        <Route path="/create" element={<Create onCreateSubmit={onCreateGameSubmit} />} />
+                    </Routes>
+                </main>
+            </div>
+        </AuthContext.Provider>
     );
 }
 
--- SoftUniCourses/ReactJs/workshop-routing/games/src/components/Login/Login.js
+++ SoftUniCourses/ReactJs/workshop-routing/games/src/components/Login/Login.js
@@ -1,15 +1,31 @@
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
+import { useForm } from "../../hooks/useForm";
+
+const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password',
+};
+
 export const Login = () => {
+
+    const { onLoginSubmit } = useContext(AuthContext);
+    const { values,changeHandler,onSubmit} = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: '',
+    },
+    onLoginSubmit,);
     return (<section id="login-page" className="auth">
-        <form id="login">
+        <form id="login" onSubmit={onSubmit}>
 
             <div className="container">
                 <div className="brand-logo"></div>
                 <h1>Login</h1>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                <input onChange={changeHandler} value={values[LoginFormKeys.Email]} type="email" id="email" name={LoginFormKeys.Email} placeholder="Sokka@gmail.com" />
 
                 <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
+                <input onChange={changeHandler} value={values[LoginFormKeys.Password]} type="password" id="login-password" name={LoginFormKeys.Password} />
                 <input type="submit" className="btn submit" value="Login" />
                 <p className="field">
                     <span>If you don't have profile click <a href="#">here</a></span>
